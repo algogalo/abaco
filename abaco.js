433ce8b4cf4a19d4c02034ffcd5e31dac072a74b
rivera-galo <51235123+rivera-galo@users.noreply.github.com>
2021-03-29 12:54:28 +0200

    update with more noise

--- abaco.js
+++ abaco.js
@@ -1,1 +1,1 @@
-shape(50,.1,.015).repeatX(()=>150*Math.sin(time/500)).add(shape(2,.001,.015)).modulate(noise(.1,.01)).out()
+shape(50,.1,.015).repeatX(()=>150*Math.sin(time/500)).add(shape(2,.001,.015)).modulate(noise(.5,.1)).out()
